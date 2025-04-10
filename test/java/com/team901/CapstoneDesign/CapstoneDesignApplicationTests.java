1c97bc17a90f699422b9dcef14ad16d3d98b2182
minyeong <my011208@gmail.com>
2025-04-10 22:44:02 +0900

    first commit

---
+++ test/java/com/team901/CapstoneDesign/CapstoneDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team901.CapstoneDesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
