374f4264fe6e84c885d051a12b9eff2fa024da49
minyeong <my011208@gmail.com>
2025-05-21 11:22:51 +0900

    build.gradle 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/build/libs/*.jar"
+JAR_FILE="$PROJECT_ROOT/capstone"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
