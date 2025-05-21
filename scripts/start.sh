d1018fafcdbec93dbbad877cd5c46d1332c49a79
minyeong <my011208@gmail.com>
2025-05-21 11:40:07 +0900

    start.sh 경로 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/capstone"
+JAR_FILE="$PROJECT_ROOT/capstone.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
@@ -9,9 +9,11 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
+LATEST_JAR=$(ls -t $PROJECT_ROOT/build/libs/capstone-0.0.1-SNAPSHOT.jar 2>/dev/null | head -n 1)
+
 # build 파일 복사
-echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+echo "$TIME_NOW > $LATEST_JAR -> $JAR_FILE 복사" >> $DEPLOY_LOG
+cp "$LATEST_JAR" "$JAR_FILE"
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
